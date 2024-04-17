bd442a5f9c9db2c1903d11939a887bfb741d7874
MateusuMelo <mateusosmar@hotmail.com>
2024-04-17 14:02:20 -0300

    Implementando função get_similar : Funcionando corretamente :)

--- server/app.py
+++ server/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, json
 import sys
-from recommender.components.recommender import get_recommendation
+from recommender.components.recommender import get_recommendation, get_similar
 
 app = Flask(__name__)
 
-@app.route("/<id>", methods=['GET'])  #Fazendo requisição diretamente pelo link. Exemplo: 127.0.0.1:5000/<Id_Usuario>
-def home(id):
-    movies = {"movies_id_list":json.loads(get_recommendation(id))}
+@app.route("/recommend/<id_user>", methods=['GET'])  #Fazendo requisição diretamente pelo link. Exemplo: 127.0.0.1:5000/<Id_Usuario>
+def recommend(id_user):
+    movies = {"movies_id_list":json.loads(get_recommendation(id_user))}
+    return movies
+
+@app.route("/similar/<id_movie>", methods=['GET'])  #Fazendo requisição diretamente pelo link. Exemplo: 127.0.0.1:5000/<Id_Usuario>
+def similar(id_movie):
+    movies = {"movies_id_list":json.loads(get_similar((id_movie)))}
     return movies
 
 if __name__ == "__main__":
