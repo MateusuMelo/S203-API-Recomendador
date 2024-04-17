92d98304690a9960eeababe050f685593915447e
MateusuMelo <mateusosmar@hotmail.com>
2024-04-17 20:52:49 -0300

    API PRONTA E FUNCIONAL

--- server/app.py
+++ server/app.py
@@ -1,17 +1,16 @@
 from flask import Flask, json
-import sys
 from recommender.components.recommender import get_recommendation, get_similar
 
 app = Flask(__name__)
 
 @app.route("/recommend/<id_user>", methods=['GET'])  #Fazendo requisição diretamente pelo link. Exemplo: 127.0.0.1:5000/<Id_Usuario>
 def recommend(id_user):
-    movies = {"movies_id_list":json.loads(get_recommendation(id_user))}
+    movies = json.loads(get_recommendation(id_user))
     return movies
 
 @app.route("/similar/<id_movie>", methods=['GET'])  #Fazendo requisição diretamente pelo link. Exemplo: 127.0.0.1:5000/<Id_Usuario>
 def similar(id_movie):
-    movies = {"movies_id_list":json.loads(get_similar((id_movie)))}
+    movies = json.loads(get_similar((id_movie)))
     return movies
 
 if __name__ == "__main__":
--- server/recommender/components/database.py
+++ server/recommender/components/database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.db_path = "../server/db.sqlite3"
+        self.db_path = "../server/db.sqlite3"  # O caminho arquivo sqlite deve ser relativo ao terminal venv
 
     def query(self, query):
         connect = sqlite3.connect(self.db_path)
